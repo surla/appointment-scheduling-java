049547f7fb153b1800f3c9f77385bda6eeffce47
Joseph Surla <josephsurla@gmail.com>
2020-09-12 23:55:11 +0900

    Create login view and controller

---
+++ src/View_Controller/LoginController.java
@@ -0,0 +1,14 @@
+package View_Controller;
+
+import java.awt.*;
+
+public class LoginController {
+    @FXML Label userNameLabel;
+    @FXML TextField usernameTextField;
+    @FXML Label passwordLabel;
+    @FXML TextField passwordTextField;
+    @FXML Button logInButton;
+
+
+
+}
