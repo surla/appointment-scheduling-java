9b8c16fafd41f273521f5dbbf0628c2558d62530
Joseph Surla <josephsurla@gmail.com>
2020-09-20 15:04:59 +0900

    Update handleLogInButton to switch scene with correct user

--- src/View_Controller/LoginController.java
+++ src/View_Controller/LoginController.java
@@ -2,12 +2,19 @@ package View_Controller;
 
 import javafx.fxml.FXML;
 
+import javafx.fxml.FXMLLoader;
+import javafx.event.ActionEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import utils.DBConnection;
+import javafx.stage.Stage;
 import utils.DBQuery;
 
 
-import java.awt.*;
+import javax.swing.*;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -25,7 +32,7 @@ public class LoginController {
     private static Connection conn = null;
 
     // Handles LoginButton click
-    public void handleLoginButton() throws SQLException {
+    public void handleLoginButton(ActionEvent event) throws SQLException, IOException {
 
         username = usernameTextField.getText();
         password = passwordTextField.getText();
@@ -45,10 +52,15 @@ public class LoginController {
         if (rs.next()) {
             System.out.println("User found!");
             System.out.println(rs);
+
+            Parent root = FXMLLoader.load(getClass().getResource("/View_Controller/Main.fxml"));
+            Scene mainScene = new Scene(root);
+            Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            window.setScene(mainScene);
+            window.show();
         } else {
             System.out.println("User not found!");
         }
 
     }
-
 }
