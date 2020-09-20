70539e4417068902ffa1d775406eb600688ce9bc
Joseph Surla <josephsurla@gmail.com>
2020-09-20 21:58:50 +0900

    Add spanish language to log in view

--- src/View_Controller/LoginController.java
+++ src/View_Controller/LoginController.java
@@ -7,7 +7,9 @@ import javafx.event.ActionEvent;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 import utils.DBQuery;
@@ -19,20 +21,31 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class LoginController {
     @FXML private TextField usernameTextField;
     @FXML private TextField passwordTextField;
+    @FXML private Label titleLabel;
+    @FXML private Label usernameLabel;
+    @FXML private Label passwordLabel;
+    @FXML private Button logInButton;
 
     // Stores username and password
     private static String username;
     private static String password;
 
+
+    private String alertTitle;
+    private String alertHeaderText;
+    private String alertContentText;
+
     // Database connection
     private static Connection conn = null;
 
     // Handles LoginButton click
-    public void handleLoginButton(ActionEvent event) throws SQLException, IOException {
+    public void handleLogInButton(ActionEvent event) throws SQLException, IOException {
 
         username = usernameTextField.getText();
         password = passwordTextField.getText();
@@ -59,8 +72,34 @@ public class LoginController {
             window.setScene(mainScene);
             window.show();
         } else {
-            System.out.println("User not found!");
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle(alertTitle);
+            alert.setHeaderText(alertHeaderText);
+            alert.setContentText(alertContentText);
+
+            alert.showAndWait();
         }
 
+    }
+
+    public void initialize() {
+        ResourceBundle rb = ResourceBundle.getBundle("utils/language", Locale.getDefault());
+
+        titleLabel.setText(rb.getString("title"));
+        usernameLabel.setText(rb.getString("username"));
+        passwordLabel.setText(rb.getString("password"));
+        logInButton.setText(rb.getString("logIn"));
+
+        alertTitle = rb.getString("alertTitle");
+        alertHeaderText = rb.getString("alertHeaderText");
+        alertContentText = rb.getString("alertContentText");
+
+
+
+//        if (Locale.getDefault().getLanguage().equals("en")) {
+//
+//        } else {
+//            System.out.println("Hello World!");
+//        }
     }
 }
